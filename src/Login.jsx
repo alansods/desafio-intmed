bfda4ba1ab8739e965b1abce945dba9420a11f67
Alan Santos <alansods@gmail.com>
2022-04-18 20:12:08 -0300

    vai

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
 
+const Button = styled.button`
+  width: 100%;
+  height: 80px;
+  background-color: blue;
+  color: white;
+  border-radius: 8px;
+  font-weight: bold;
+`
+
 export default function App() {
   return (
     <div className="App">
       <h1>Login</h1>
+      <Button />
     </div>
   );
 }
